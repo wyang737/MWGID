a58247aac86bdc21c569fd3cee2856289f3b775d
Nilesh <nileshmukherji@gmail.com>
2021-05-04 20:58:35 -0400

    deleting comments

--- front-end/frontend/src/app/insert/insert.component.ts
+++ front-end/frontend/src/app/insert/insert.component.ts
@@ -23,31 +23,4 @@ export class InsertComponent implements OnInit {
   } 
 }
 
-/*
-export class InsertComponent implements OnInit {
-	SERVER_URL = "http://localhost:4200/insert";
-	form: FormGroup;
-  constructor(private FormBuilder: FormBuilder) {
-  	this.form = this.FormBuilder.group({
-  		data: ['']
-  	})
-   }
 
-  ngOnInit(){
-  }
-  uploadText(event){
-  	const text = (event.target as HTMLInputElement)
-  }
-  submitForm(){
-  	var formData: any = new FormData()
-  	formData.append("userid", this.form.get('userid').value);
-  	formData.append("recipid", this.form.get('recipid').value);
-  	formData.append("data", this.form.get('data').value);
-  	this.http.post('')
-  }
-}
-<<<<<<< HEAD:Front-End/frontend/src/app/insert/insert.component.ts
-*/
-=======
-*/
->>>>>>> 50685ea3397221a7d20d2227c9c447f28265103e:front-end/frontend/src/app/insert/insert.component.ts
