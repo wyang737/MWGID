8e7d62130dc32cb7baf3fcf239466e3e753c3d00
Winston Yang <wyang737@users.noreply.github.com>
2021-05-17 18:23:21 -0400

    test file

---
+++ test.js
@@ -0,0 +1,43 @@
+var http = require('http')
+
+http.get('http://localhost:4200/api/insert/?userID=specialtest&recipID=number&data=' + '420', function(response) {
+	http.get('http://localhost:4200/api/query/?userID=specialtest', function(res) {
+		var specialBody;
+		res.on("data", function(chunk) {
+			console.log("Testing single insertion:");
+			specialBody += chunk;	
+		})
+		res.on("end", () =>{
+			var jsonRet = JSON.parse(specialBody.substring(9));
+			if (jsonRet.transactions[0].recipID == "number" && jsonRet.transactions[0].data == '420')
+			{
+				console.log("Single Insert Worked")
+			}
+		})
+	})
+})
+
+var i;
+for (i = 0; i <= 10; i ++)
+{
+	http.get('http://localhost:4200/api/insert/?userID=test&recipID=number&data=' + i), (response) => {
+	}
+} 
+
+http.get('http://localhost:4200/api/queryall', function(response) {
+});
+
+http.get('http://localhost:4200/api/query/?userID=hash', function(res) {
+    var hashBody;
+	res.on("data", function(chunk) {
+		console.log("Testing for hashing/queryall:");
+		hashBody += chunk;	
+	})
+	res.on("end", () =>{
+		var jsonHashRet = JSON.parse(hashBody.substring(9));
+		if (jsonHashRet.transactions[0].userID == "hash")
+		{
+			console.log("Hash / Queryall worked")
+		}
+	})
+});
