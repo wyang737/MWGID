b09febe03b1cf87b853b5b1589fb5bb630ab8cd0
Nilesh <nileshmukherji@gmail.com>
2021-12-14 17:44:02 -0500

    Finalizing custom CI/CD pipeline dev

--- updater.py
+++ updater.py
@@ -1,6 +1,8 @@
 import requests
 
 URL = "http://3.17.77.198:5000/"
+#URL = "http://localhost:5000/"
 password = "UpdateApplicationXYZ123!!"
 
-requests.get(URL, params={'password':password})
+text = requests.get(URL, params={'password':password})
+print(text.text)
